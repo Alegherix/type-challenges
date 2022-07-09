e024230c06eae00e73ac89b8a3bda00d7e4ab733
Martin <alegherix@gmail.com>
2022-07-09 20:03:41 +0200

    Added solution to awaiting promises

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,4 @@
-type MyAwaited = any
+// Om vi har en typ som Extendar ett löfte,
+// Försök få ut dess Type inuti Promiset och recursa nedåt,
+// Om vi ej extendar ett Promise längre, returnera bara typen.
+type MyAwaited<T> = T extends Promise<infer R> ? MyAwaited<R> : T
