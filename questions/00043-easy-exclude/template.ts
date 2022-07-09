1e706886cf606287e0887215dc56ae6e62a49280
Martin <alegherix@gmail.com>
2022-07-09 16:44:32 +0200

    Added solution to Exclude

--- questions/00043-easy-exclude/template.ts
+++ questions/00043-easy-exclude/template.ts
@@ -1,1 +1,3 @@
-type MyExclude<T, U> = any
+// If T is in the same set as U, return nothing, otherwise we know that the types are different
+// The distribution will make sure the ending type is a union type.
+type MyExclude<T, U> = T extends U ? never : T
