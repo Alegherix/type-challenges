7e2131228c8bf7013a2a5e83dc9468bf56393d72
Martin <alegherix@gmail.com>
2022-07-09 23:22:03 +0200

    added Parameters

--- questions/03312-easy-parameters/template.ts
+++ questions/03312-easy-parameters/template.ts
@@ -1,1 +1,10 @@
-type MyParameters<T extends (...args: any[]) => any> = any
+type MyParameters<T extends (...args: any[]) => any> = T extends (
+  ...args: infer P
+) => any
+  ? P
+  : never
+
+type ArrayToUnion<T extends any[]> = T extends (infer P)[] ? P : T
+
+type NameArrayType = ['Carl', 'Blake', 'Adam']
+type NameUnion = ArrayToUnion<NameArrayType>
