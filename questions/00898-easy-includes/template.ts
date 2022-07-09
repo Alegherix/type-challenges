de3ea472236f48d89f94d7660fa8b0def9ab272d
Martin <alegherix@gmail.com>
2022-07-09 22:06:09 +0200

    Added Includes

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,17 @@
-type Includes<T extends readonly any[], U> = any
+// GOAL
+
+// Infer the Key from the first elemen in the array, and then infer the rest
+// If the infered value is equal to the second generic params, then that value is in the array, thus return true
+// Otherwise recurse down and keep trying the remaining parts of the array,
+// If we can't infer, I.e no more elemens, return false.
+type Includes<T extends readonly any[], U> = T extends [infer K, ...infer R]
+  ? IsEqual<K, U> extends true
+    ? true
+    : Includes<R, U>
+  : false
+
+type IsEqual<T, K> = (<P>() => P extends T ? 1 : 2) extends <P>() => P extends K
+  ? 1
+  : 2
+  ? true
+  : false
