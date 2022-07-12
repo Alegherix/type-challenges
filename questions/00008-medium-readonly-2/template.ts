05670ebb2f4172c4579af3d059d53918a3746992
Martin <alegherix@gmail.com>
2022-07-12 20:49:26 +0200

    Completed readonly2

--- questions/00008-medium-readonly-2/template.ts
+++ questions/00008-medium-readonly-2/template.ts
@@ -1,1 +1,28 @@
-type MyReadonly2<T, K> = any
+type MyReadonly1<T, U extends keyof T = keyof T> = {
+  readonly [K in U]: T[K]
+} & Omit<T, U>
+
+type MyReadonly2<T, U extends keyof T = keyof T> = {
+  readonly [K in U]: T[K]
+} & {
+  [K in keyof T as K extends U ? never : K]: T[K]
+}
+
+type A = 'A' & 'B' & 'C'
+
+const objA: A = {}
+
+type B = 'B'
+
+type C = A & B
+
+type Person = { name: string; age: number }
+type Animal = { length: number }
+
+type AnimalPerson = Person & Animal
+
+const obj: AnimalPerson = {
+  age: 12,
+  length: 12,
+  name: '232323',
+}
