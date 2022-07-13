a409084e534072d4fd72342a4cba80ac98e33e0f
Martin <alegherix@gmail.com>
2022-07-13 20:31:08 +0200

    Added Tuple to union

--- questions/00008-medium-readonly-2/template.ts
+++ questions/00008-medium-readonly-2/template.ts
@@ -7,22 +7,3 @@ type MyReadonly2<T, U extends keyof T = keyof T> = {
 } & {
   [K in keyof T as K extends U ? never : K]: T[K]
 }
-
-type A = 'A' & 'B' & 'C'
-
-const objA: A = {}
-
-type B = 'B'
-
-type C = A & B
-
-type Person = { name: string; age: number }
-type Animal = { length: number }
-
-type AnimalPerson = Person & Animal
-
-const obj: AnimalPerson = {
-  age: 12,
-  length: 12,
-  name: '232323',
-}
--- questions/00009-medium-deep-readonly/template.ts
+++ questions/00009-medium-deep-readonly/template.ts
@@ -1,1 +1,22 @@
-type DeepReadonly<T> = any
+type DeepReadonly<T> = T extends Function
+  ? T
+  : { readonly [K in keyof T]: DeepReadonly<T[K]> }
+
+type User = {
+  name: string
+  age: () => number
+  property: {
+    getLength: () => 11
+  }
+}
+
+type ReadUser = Readonly<User>
+type T = Readonly<() => 22>
+
+type DeepReadonly2<T> = T extends Function
+  ? T
+  : {
+      [K in keyof T]: Readonly<DeepReadonly2<T[K]>>
+    }
+
+type UserTest = DeepReadonly2<User>
