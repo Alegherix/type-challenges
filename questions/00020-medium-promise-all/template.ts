715bcca3ee781ba68926686626da4ebd96a61434
Martin <alegherix@gmail.com>
2022-07-15 00:21:34 +0200

    Added PromiseAll

--- questions/00020-medium-promise-all/template.ts
+++ questions/00020-medium-promise-all/template.ts
@@ -1,1 +1,13 @@
-declare function PromiseAll(values: any): any
+declare function PromiseAll<T extends any[]>(
+  values: readonly [...T]
+): Promise<{
+  [K in keyof T]: T[K] extends Promise<infer R> ? R : T[K]
+}>
+
+type User = {
+  name: number
+  associates: {
+    name: 'stan'
+    age: 24
+  }
+}
